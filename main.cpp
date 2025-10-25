7abbf85e9779017e5fa8feb27af0ad26c3da22ee
rezch <a@b>
2025-10-25 22:33:33 +0300

    r

--- main.cpp
+++ main.cpp
@@ -1,23 +1,19 @@
 #include <iostream>
 #include <curl/curl.h>
+#include <thread>
 
 
 int ping_callback(CURL*, void*, int action)
 {
-    switch (action) {
-    case CURL_PING_SEND:
-        std::cout << "=====> GOT: CURL_PING_SEND\n";
-        break;
-    case CURL_PING_SENDERR:
-        std::cout << "=====> GOT: CURL_PING_SENDERR\n";
-        return CURLE_PING_TIMEOUT;
-    case CURL_PING_RECV:
-        std::cout << "=====> GOT: CURL_PING_RECV\n";
-        break;
-    case CURL_PING_RECVACK:
-        std::cout << "=====> GOT: CURL_PING_RECVACK\n";
-        break;
-    }
+    const char* msg[] = {
+        "CURL_PING_SEND",
+        "CURL_PING_SENDERR",
+        "CURL_PING_RECV",
+        "CURL_PING_RECVACK",
+        "CURL_PING_CONNOPEN",
+        "CURL_PING_CONNCLOSE",
+    };
+    std::cout << "=====> GOT: " << msg[action - 1] << "\n";
     return CURLE_OK;
 }
 
@@ -33,12 +29,19 @@ int main()
 
     curl_easy_setopt(curl, CURLOPT_URL, url);
     curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
+    curl_easy_setopt(curl, CURLOPT_MAXAGE_CONN, 1L);
     curl_easy_setopt(curl, CURLOPT_UPKEEP_INTERVAL_MS, 0L);
     curl_easy_setopt(curl, CURLOPT_PING_FUNCTION, ping_callback);
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
 
     curl_easy_perform(curl);
     curl_easy_upkeep(curl);
+
+    curl_easy_setopt(curl, CURLOPT_FRESH_CONNECT, 1L);
     curl_easy_perform(curl);
+
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+    curl_easy_perform(curl);
+
     curl_easy_cleanup(curl);
 }
